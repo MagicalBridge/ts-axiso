83daeef6fa1a992911f498d13944bd1440f1b3dd
褚鹏飞 <540788769@qq.com>
2022-12-12 12:28:25 +0800

    docs(在script中添加一个启动命令): 添加npm run dev

--- src/xhr.ts
+++ src/xhr.ts
@@ -4,7 +4,7 @@ export default function xhr(config: AxiosRequestConfig): void {
   const { data = null, url, method = 'get' } = config
 
   const request = new XMLHttpRequest()
-
+  // 第一个参数 指明方法，默认情况下是大写，第三个参数是同步还是异步，传入true表明是异步
   request.open(method.toLowerCase(), url, true)
 
   request.send(data)
